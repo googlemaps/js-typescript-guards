3285e4d9f16e29a76e64c46ec08137d5eb0af843
Justin Poehnelt <jpoehnelt@google.com>
2021-11-15 16:37:25 -0700

    build: update rollup dev dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 
 export default [
   {
